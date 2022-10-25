02b00633b60ff47af006b5153f54be5a61135a2e
dberten <dorian.berten@epitech.eu>
2022-10-25 15:41:58 +0200

    [+] add schemas, controllers and routes

---
+++ api/lib/api_web/views/clock_view.ex
@@ -0,0 +1,18 @@
+defmodule APIWeb.ClockView do
+  use APIWeb, :view
+  alias APIWeb.ClockView
+
+  def render("index.json", %{clocks: clocks}) do
+    %{data: render_many(clocks, ClockView, "clock.json")}
+  end
+
+  def render("show.json", %{clock: clock}) do
+    %{data: render_one(clock, ClockView, "clock.json")}
+  end
+
+  def render("clock.json", %{clock: clock}) do
+    %{id: clock.id,
+      time: clock.time,
+      status: clock.status}
+  end
+end
---
+++ api/lib/api_web/views/workingtime_view.ex
@@ -0,0 +1,18 @@
+defmodule APIWeb.WorkingtimeView do
+  use APIWeb, :view
+  alias APIWeb.WorkingtimeView
+
+  def render("index.json", %{workingtimes: workingtimes}) do
+    %{data: render_many(workingtimes, WorkingtimeView, "workingtime.json")}
+  end
+
+  def render("show.json", %{workingtime: workingtime}) do
+    %{data: render_one(workingtime, WorkingtimeView, "workingtime.json")}
+  end
+
+  def render("workingtime.json", %{workingtime: workingtime}) do
+    %{id: workingtime.id,
+      start: workingtime.start,
+      end: workingtime.end}
+  end
+end
